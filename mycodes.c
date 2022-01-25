5c61ad7f014e8078bd24233e734003f653e1335f
segin <98380527+seginbytes@users.noreply.github.com>
2022-01-26 00:18:35 +0530

    Update mycodes.c
    using strings

--- mycodes.c
+++ mycodes.c
@@ -1,7 +1,10 @@
 # include <stdio.h>
+# include <cs50.h>
 
 int main ()
 {
-    printf("hello,world\n");
+    string answer = get_string ("what is your name? ");
+    printf("hello,%s\n",answer);
     return 0;
 }
+
