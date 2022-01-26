1af9fa50c3ef081a2aa873b529c43b6b20d07ab0
segin <98380527+seginbytes@users.noreply.github.com>
2022-01-26 11:28:08 +0530

    Update dicount.c
    faster

--- dicount.c
+++ dicount.c
@@ -5,8 +5,7 @@ int main()
 {
     float cost = get_float ("what is the cost ?");
     float discount = get_float ("percentage of discount :  ");
-    float ans = cost*discount;
-            ans = ans / 100;
-             ans = cost - ans;
-    printf("dicount price = %f,\n",ans);
+    float ans = cost * discount / 100;
+    
+    printf("dicount price = %f,\n",cost - ans);
 }
