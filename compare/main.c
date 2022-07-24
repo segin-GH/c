c3460f13ae5b4279890ec338e7050d9d1b5779cf
segin-GH <segin.bytes@gmail.com>
2022-07-24 23:51:47 +0530

    .

---
+++ compare/main.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <string.h>
+
+
+int main(void)
+{
+    char *s = "segin";
+    char *t = "sebin";
+
+    if(strcmp(s,t) == 0){
+
+        printf("ah same \n");
+    }
+    else {
+
+        printf("not smae \n");
+    }
+  
+}
