b2c053cefbb9b5ffd166ea2789bf192b6c58c76e
segin-GH <segin.bytes@gmail.com>
2022-07-24 23:52:19 +0530

    .

---
+++ pointer arth/main.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main ()
+{
+    int array_of_numbers [] = {1,2,3,4,5,6,7,8};
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%i\n",*(array_of_numbers + i));
+    }
+
+}
