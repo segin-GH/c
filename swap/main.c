508ba0c39fd02f8ef2ac5d9ce4976f2f5909647d
segin-GH <segin.bytes@gmail.com>
2022-07-25 04:02:33 +0530

    .

---
+++ swap/main.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+// # include <stdlib.h>
+
+void swap(int *x, int *y);
+
+int main(void)
+{
+    int x = 10, y = 20;
+
+    printf("%i  %i  \n",x,y);
+
+    swap( &x, &y);      // passing by refrence
+
+    printf("%i  %i  \n",x,y);
+}
+
+
+void swap(int *x, int *y)
+{
+    int temp = *x;      
+    *x = *y;
+    *y = temp;
+    
+}
