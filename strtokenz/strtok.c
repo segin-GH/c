a677f56b68c8c65acddb570d3f1271d1f5753085
segin-GH <segin.bytes@gmail.com>
2023-04-03 18:11:14 +0530

    .

--- strtokenz/strtok.c
+++ strtokenz/strtok.c
@@ -4,18 +4,21 @@
 
 #define MAX_VALUES 100 // Maximum number of values we can handle
 
-int main() {
+int main()
+{
     char hex_string[] = "0xff, 0x00, 0x00, 0x00";
     int values[MAX_VALUES] = {0};
     int num_values = 0;
-    char* token = strtok(hex_string, ", ");
+    char *token = strtok(hex_string, ", ");
 
-    while (token != NULL && num_values < MAX_VALUES) {
+    while (token != NULL && num_values < MAX_VALUES)
+    {
         values[num_values++] = (int)strtol(token, NULL, 16);
         token = strtok(NULL, ", ");
     }
 
-    for (int i = 0; i < num_values; i++) {
+    for (int i = 0; i < num_values; i++)
+    {
         printf("%d ", values[i]);
     }
 
