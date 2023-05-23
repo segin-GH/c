cdc58507cf864e6dd0832ddd3ac2920dbc6fec2f
segin-GH <segin.bytes@gmail.com>
2023-05-23 20:52:34 +0530

    .

--- H_S private/Question1(swap using pointers)/main.c
+++ H_S private/Question1(swap using pointers)/main.c
@@ -1,42 +1,39 @@
-# include <stdio.h>
+#include <stdio.h>
 
-void print_array(int array [], int size);
-void  swap_array_using_pointer(int* array, int size);
+void print_array(int array[], int size);
+void swap_array_using_pointer(int *array, int size);
 
-
-int main (void)
+int main(void)
 {
-    int arraySize;  
+    int arraySize;
     printf("How many numbers do you have : ");
     scanf("%i", &arraySize);
-    int array[arraySize];                                    // intialized a array;
-    
-    for(int i = 0; i < arraySize; i++)
-    {   
+    int array[arraySize]; // intialized a array;
+
+    for (int i = 0; i < arraySize; i++)
+    {
         int temp;
-        printf("Enter your %i element : ",i);
+        printf("Enter your %i element : ", i);
         scanf("%i", &temp);
         array[i] = temp;
     }
 
-    print_array(array,arraySize);               
-    int* p = array;                             // intitalied a pointer 
-    swap_array_using_pointer(p,arraySize);      //swaped the array using the function call 
-    print_array(array,arraySize);               // printed the array
-
+    print_array(array, arraySize);
+    int *p = array;                         // intitalied a pointer
+    swap_array_using_pointer(p, arraySize); // swaped the array using the function call
+    print_array(array, arraySize);          // printed the array
 }
 
-void print_array(int array [], int size)
+void print_array(int array[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        printf("%i", array [i]);
+        printf("%i", array[i]);
     }
     printf("\n");
 }
 
-
-void  swap_array_using_pointer(int* array, int size)
+void swap_array_using_pointer(int *array, int size)
 {
     // printf("%i\n",*p);
 
@@ -49,5 +46,4 @@ void  swap_array_using_pointer(int* array, int size)
         array[i] = array[j];
         array[j] = temp;
     }
-    
 }
