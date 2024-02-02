e3223495aa869bcec422e740d1b651ed65a05933
segin <98380527+segin-GH@users.noreply.github.com>
2024-02-02 19:45:52 +0530

    Add files via upload

---
+++ bin_printer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // adding this for the sleep function, because we're taking naps now
+
+int main() {
+    FILE *filePtr;
+    unsigned char byte;
+    int counter = 0;
+
+    filePtr = fopen("ota.bin", "rb"); // "yourfile.bin" - hoping it's not just a diary of bad codebbb
+
+    if (filePtr == NULL) {
+        fprintf(stderr, "tried opening the void. file not found.");
+        return EXIT_FAILURE;
+    }
+
+    while (fread(&byte, sizeof(byte), 1, filePtr)) {
+        printf("%02x ", byte);
+        counter++;
+
+        if (counter == 16) {
+            printf("\n");
+            sleep(1); // here's your dramatic pause, don't get too emotional
+            counter = 0;
+        }
+    }
+
+    fclose(filePtr);
+    return 0;
+}
+
