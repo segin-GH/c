85de10b5b512a64e49575144c90c5e6022c93507
segin-GH <segin.bytes@gmail.com>
2024-04-28 02:12:15 +0530

    style: formated the code

--- bin_printer.c
+++ bin_printer.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h> // adding this for the sleep function, because we're taking naps now
 
-int main() {
+int main()
+{
     FILE *filePtr;
     unsigned char byte;
     int counter = 0;
 
     filePtr = fopen("ota.bin", "rb"); // "yourfile.bin" - hoping it's not just a diary of bad codebbb
 
-    if (filePtr == NULL) {
+    if (filePtr == NULL)
+    {
         fprintf(stderr, "tried opening the void. file not found.");
         return EXIT_FAILURE;
     }
 
-    while (fread(&byte, sizeof(byte), 1, filePtr)) {
+    while (fread(&byte, sizeof(byte), 1, filePtr))
+    {
         printf("%02x ", byte);
         counter++;
 
-        if (counter == 16) {
+        if (counter == 16)
+        {
             printf("\n");
             counter = 0;
         }
@@ -26,5 +30,3 @@ int main() {
 
     fclose(filePtr);
     return 0;
-}
-
