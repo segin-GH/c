8a26b69e72776c6b303cf3ba13a2f8f8dca26682
segin-GH <segin.bytes@gmail.com>
2024-05-26 22:47:44 +0530

    chore: clean up

---
+++ 2d_array/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, j = 0;
+    int arr[4][2] = {{1000, 100}, {1001, 101}, {1002, 102}, {1003, 103}};
+
+    for (i = 0; i < 4; i++)
+    {
+        {
+            printf("device %d = Device ID [%d] SRC ID [%d] \n", i, arr[i][0], arr[i][1]);
+        }
+    }
+    return 0;
+}
--- tree/main.c
+++ tree/main.c
@@ -1,50 +1,44 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node
 {
     int number;
-    struct node* left;
-    struct node* right;
-}
-node;
+    struct node *left;
+    struct node *right;
+} node;
 
-
-void print_tree(node* root);
-void free_tree(node* root);
-
-
+void print_tree(node *root);
+void free_tree(node *root);
 
 int main()
 {
 
     node *tree = NULL;
 
-    node* n = malloc(sizeof(node));
-    
-    if( n == NULL)
-    {   
+    node *n = malloc(sizeof(node));
+
+    if (n == NULL)
+    {
         printf("no space 1 \n");
         return 1;
     }
-    n -> number  = 2;
-    n -> left = NULL;
-    n -> right = NULL;
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
     tree = n;
 
-
     n = malloc(sizeof(node));
-    
+
     if (n == NULL)
     {
         printf("no space 2\n");
         return 1;
     }
-    n -> number = 1;
-    n -> left = NULL;
-    n -> right = NULL;
-    tree -> left = n;
-
+    n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    tree->left = n;
 
     n = malloc(sizeof(node));
     if (n == NULL)
@@ -52,32 +46,26 @@ int main()
         printf("no space 3 \n");
         return 1;
     }
-    n -> number = 3;
-    n -> left = NULL;
-    n -> right = NULL;
-    tree -> right = n;
+    n->number = 3;
+    n->left = NULL;
+    n->right = NULL;
+    tree->right = n;
 
-    
     print_tree(tree);
 
     free_tree(tree);
-    
-     
-
 }
 
 void free_tree(node *root)
 {
-    if(root == NULL)
+    if (root == NULL)
     {
         return;
     }
-    free_tree(root -> left);
-    free_tree(root -> right);
+    free_tree(root->left);
+    free_tree(root->right);
     free(root);
-
 }
-
 
 void print_tree(node *root)
 {
@@ -86,8 +74,7 @@ void print_tree(node *root)
         return;
     }
 
-    print_tree(root -> left);
-    printf("%i\n",root -> number);
-    print_tree(root -> right);
-
+    print_tree(root->left);
+    printf("%i\n", root->number);
+    print_tree(root->right);
 }
